1e778016ca81823bfbbbc076fdb765efbd0de6e5
Christina Sohn <chsohn15@gmail.com>
2020-05-22 18:03:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction_with_language(name, language)
   puts "Hi name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with language_optional(name, language = "Ruby")
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi name is #{name} and I am learning to program in #{language}."
 end
 
